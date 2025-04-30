fe9ebf1692410ed0201d16b589b80b2ff2f5fc92
Matheus Luppi <matluppi@gmail.com>
2025-04-30 20:06:57 -0300

    Fix import path for Render

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import sys
 # DON'T CHANGE THIS !!!
-sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
 
 from flask import Flask, send_from_directory, request, jsonify
 from flask_cors import CORS # Importar CORS
